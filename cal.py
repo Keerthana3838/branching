8aacb4bc037ec63bfa5a2fcec41abccb970876ad
Keerthana3838 <mkeerthana594@gmail.com>
2025-03-12 12:16:42 +0530

    adding subtract method

--- cal.py
+++ cal.py
@@ -5,3 +5,13 @@ if __name__ == "__main__":
     num1 = int(input("Enter first number: "))
     num2 = int(input("Enter second number: "))
     print("Addition:", add(num1, num2))
+
+
+def subtract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    print("Addition:", add(num1, num2))
+    print("Subtraction:", subtract(num1, num2))
